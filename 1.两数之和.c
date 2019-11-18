5e910b62703f9e93f6ff6a5c9cf7042357666bb1
LLLebin <43530538+LLLbin@users.noreply.github.com>
2019-11-18 22:59:42 +0800

    master

--- 1.两数之和.c
+++ 1.两数之和.c
@@ -2,7 +2,7 @@
  * @Author: Lebin
  * @Date: 2019-11-17 02:16:52
  * @LastEditors: Lebin
- * @LastEditTime: 2019-11-18 21:46:11
+ * @LastEditTime: 2019-11-18 22:58:51
  * @Description: 
  */
 /*
@@ -15,22 +15,23 @@
 
 
 /**
+ * Return an array of size *returnSize.//告诉调用者返回的数组的大小
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    for (int i = 0 ; i < numsSize; i++)
-    {
-        for (int j = i + 1; j < numsSize; j++)
-        {
-            if (nums[i] + nums[j] == target)
-            {
-                returnSize[0] = nums[i];
-                returnSize[1] = nums[j];
-                return returnSize;
+    int* res = (int *)malloc(sizeof(int) * 2);
+    * returnSize=0;
+    for(int i = 0; i < numsSize-1; i++) {
+        for(int j = i + 1; j < numsSize; j++) {
+            if(nums[i] + nums [j] == target) {
+                res[0] = i;
+                res[1] = j;
+                *returnSize = 2;
+                return res;
             }
         }
     }
-    return 0;
+    return res;
 }
         
 
