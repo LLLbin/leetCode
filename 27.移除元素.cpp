64f1816e303154937665060a1ef08f365aa6054e
LLLebin <43530538+LLLbin@users.noreply.github.com>
2020-05-18 18:54:04 +0800

    master

---
+++ 27.移除元素.cpp
@@ -0,0 +1,22 @@
+/*
+ * @Author: Lebin
+ * @Date: 2020-05-18 13:20:48
+ * @LastEditors: Lebin
+ * @LastEditTime: 2020-05-18 14:59:51
+ * @Description: 
+ */ 
+/*
+ * @lc app=leetcode.cn id=27 lang=cpp
+ *
+ * [27] 莽搂禄茅鈩⒙っモ€ζ捗�麓聽
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+    }
+};
+// @lc code=end
+
